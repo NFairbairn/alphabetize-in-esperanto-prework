f698f9b1ebbbd0c2b71a3b6cba6cf3e0d180addb
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-10 20:47:13 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,6 @@ require 'pry'
 
 def alphabetize(arr)
   # code here
-  ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
 end
