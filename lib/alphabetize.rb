4dd65fce7c6326a4d23ea6854611ae3bf7f35ea4
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-10 20:29:08 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,6 +4,7 @@ def alphabetize(arr)
   # code here
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   chopped = esperanto_alphabet.split(//)
-  sorted = arr.sort_by{|word| word}
-  binding.pry  
+  sorted = arr.sort_by{|word| }
+  arr.sort
+  binding.pry
 end
