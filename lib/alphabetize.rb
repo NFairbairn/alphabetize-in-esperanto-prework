0474b554fbf3ddbd8e017f94f3d01ecb4ca13901
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-10 20:36:28 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 def alphabetize(arr)
   # code here
+  english_alphabet = "abcdefghijklmnopqrstuvwxyz"
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   chopped = esperanto_alphabet.split(//)
   chopped.each_with_index do |letter, index|
