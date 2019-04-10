f418aa8f792914105450dc302d754e1632ac6591
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-10 20:13:45 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,6 +4,6 @@ def alphabetize(arr)
   # code here
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-   chopped = esperanto_alphabet.split(//).map_with_index{|letter, index| "#{index}.#{letter}"}
+   chopped = esperanto_alphabet.split(//).each_with_index{|letter, index| "#{index}.#{letter}"}
    binding.pry
 end
