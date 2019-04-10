d3b19d9aca82d4940535413b888e3d9b01b002b7
Noah Fairbairn <noahfairbairn@gmail.com>
2019-04-10 20:16:34 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -7,7 +7,7 @@ def alphabetize(arr)
    chopped = esperanto_alphabet.split(//)
      chopped.each_with_index do |letter, index|
        binding.pry
-        "#{index}.#{letter}"
+        
      end
 
 end
